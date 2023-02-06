0428f61158b390bc8048a0c1151b28801578fa81
GoninDS <luisdi2509@gmail.com>
2023-02-06 15:50:28 -0600

    Started initial testing with Player class and window

--- src/Player.py
+++ src/Player.py
@@ -1,23 +1,24 @@
+import pygame
+
 # Player object
-
 class Player:
-  def __init__(self, player_number):
+  def __init__(self, player_number, image_name):
     self.player_number = player_number
+    # Set the desired x and y coordinates
     if player_number == 0:
-      self.x = 0
-      self.y = 0
+      self.x = 100
+      self.y = 100
     else:
-      self.x = 10
-      self.y = 10
+      self.x = 100
+      self.y = 100
+    # Load image
+    self.image = pygame.image.load(image_name)
 
-  def blit_image():
-    pass
+  def blit_image(self, screen):
+    screen.blit(self.image, (self.x, self.y))
 
   def move_upwards(self):
     pass
 
   def move_downwards(self):
     pass
-
-player0 = Player(0)
-player1 = Player(1)
--- src/main.py
+++ src/main.py
@@ -1,16 +1,26 @@
+# Import pygame
 import pygame
+# Import the player object
+from Player import Player
 
-# Initialize pygame, otherwise the functions won't work
+# Initialize pygame
 pygame.init()
 
-screen = pygame.display.set_mode((800,600))
+# Setup the screen
+screen = pygame.display.set_mode((1200, 600))
+pygame.display.set_caption("Pong")
+
+first_player = Player(1, 'alien.png')
 
 # Game loop
-# Variable needed for while true loop
 continue_running = True
 while continue_running:
   # For loop to go through all events in pygame
   for event in pygame.event.get():
-    # Event to quit found, then quit
+    # Event to quit
     if event.type == pygame.QUIT:
       continue_running = False
+
+    # Window must be updated
+    first_player.blit_image(screen)
+    pygame.display.update()
